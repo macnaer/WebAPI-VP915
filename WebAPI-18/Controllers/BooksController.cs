d58e4a822433ea08a18f01db47d8faef1ec005c7
trofimchuk <trofimchuk@itstep.academy>
2021-08-18 20:44:41 +0300

    CRUD books

--- WebAPI-18/Controllers/BooksController.cs
+++ WebAPI-18/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI_18.Data.Models;
 using WebAPI_18.Data.Services;
 
 namespace WebAPI_18.Controllers
@@ -23,6 +24,27 @@ namespace WebAPI_18.Controllers
         {
             var allBooks = _bookService.GetAllBooks();
             return Ok(allBooks);
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            _bookService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book/{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            var updatedBook = _bookService.UpdateBook(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _bookService.DeleteBook(id);
+            return Ok();
         }
     }
 }
--- WebAPI-18/Data/Services/BookService.cs
+++ WebAPI-18/Data/Services/BookService.cs
@@ -15,5 +15,43 @@ namespace WebAPI_18.Data.Services
         }
 
         public List<Book> GetAllBooks() => _db.Books.ToList();
+
+        public void AddBook(Book book)
+        {
+            _db.Books.Add(book);
+            _db.SaveChanges();
+        }
+
+        public Book UpdateBook(int bookId, Book book)
+        {
+            var _book = _db.Books.FirstOrDefault(n => n.Id == bookId);
+
+            if(_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genre = book.Genre;
+                _book.ImageURL = book.ImageURL;
+                _book.DateAdded = book.DateAdded;
+                _book.Author = book.Author;
+                _db.SaveChanges();
+            }
+
+            return _book;
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            var _book = _db.Books.FirstOrDefault(n => n.Id == bookId);
+            
+            if(_book != null)
+            {
+                _db.Books.Remove(_book);
+                _db.SaveChanges();
+            }
+        }
     }
 }
